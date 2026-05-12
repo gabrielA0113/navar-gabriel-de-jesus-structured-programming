96c0fdaa753774d74a9cf8e03f2caf56f684c230
Gabriel Navar <navargabriel8@gmail.com>
2026-05-11 19:06:51 -0600

    Add files via upload

---
+++ UNIT 1/Practica 1 LAB PROGRA.py
@@ -0,0 +1,21 @@
+
+numbers = [1,2,3,4,5]
+squares = list(map(lambda x: x**2, numbers))
+print(squares)
+square=float 
+[1,4,9,16,25]
+
+
+grades= (80,90,70)
+average= sum(grades)/len(grades)
+print(average) 
+[80.0]
+
+
+
+def calculate_average(data):
+  return sum(data)/ len(data)
+grades=[80,90,70]
+print (calculate_average(grades))
+grades=input(float)
+[80.0]
